25b609d268d3754d78aba3bace778a251e448dd1
SaurabhSahu01 <saurabhsahu794@gmail.com>
2023-05-31 14:17:09 +0530

    [UI] - table beautified

--- components/Search/Table.jsx
+++ components/Search/Table.jsx
@@ -1,42 +1,36 @@
 import React from 'react'
-
-function Table({data}) {
-  return (
-    <div className='w-screen mt-5'>
-        <div className='w-full h-fit flex justify-center items-center gap-2 bg-slate-50 border-[1px] border-black font-medium'>
-            <div className='h-fit py-3 border-r-[1px] border-black flex'>
-                <span className='bg-green-100'>Year</span>
-            </div>
-            <div className='w-[1/3] flex flex-col justify-around items-center border-r-[1px] border-black'>
-                <span className='bg-blue-100'>Asst. Prof.</span>
-                <div className='w-full flex justify-around items-center'>
-                    <span className='bg-yellow-100'>CutOff Mark/%</span>
-                    <span className='bg-red-100'>Total</span>
-                </div>
-            </div>
-            <div className='w-[1/3] flex flex-col justify-around items-center'>
-                <span className='bg-blue-100'>JRF & Asst. Prof.</span>
-                <div className='w-full flex justify-around items-center'>
-                    <span className='bg-yellow-100'>CutOff Mark/%</span>
-                    <span className='bg-red-100'>Total</span>
-                </div>
-            </div>
+import styles from "./styles.css";
+function Table({ data }) {
+    return (
+        <div className='w-screen mt-5'>
+            <table className='w-full'>
+                <tr>
+                    <th rowspan="2">Year</th>
+                    <th colspan="2">Assistant Prof</th>
+                    <th colspan="2">JRF</th>
+                </tr>
+                <tr>
+                    <th>Cut-off Marks/%/%ile</th>
+                    <th>Total</th>
+                    <th>Cut-off Marks/%/%ile</th>
+                    <th>Total</th>
+                </tr>
+                {
+                    data?.map((item, index) => {
+                        return (
+                            <tr key={index}>
+                                <td>{item.Year}</td>
+                                <td>{item.AsstProfCutOffPercentile}</td>
+                                <td>{item.AsstProfTotal}</td>
+                                <td>{item.JRFAsstProfCutOffPercentile}</td>
+                                <td>{item.JRFAsstProfTotal}</td>
+                            </tr>
+                        )
+                    })
+                }
+            </table>
         </div>
-        {
-            data?.map((item, index) => {
-                return(
-                    <div className='grid grid-cols-5 place-items-center border-[0.5px] border-black text-sm gap-2 py-2' key={index}>
-                        <span className='bg-green-100'>{item.Year}</span>
-                        <span className='bg-yellow-100'>{item.AsstProfCutOffPercentile}</span>
-                        <span className='bg-red-100'>{item.AsstProfTotal}</span>
-                        <span className='bg-yellow-100'>{item.JRFAsstProfCutOffPercentile}</span>
-                        <span className='bg-red-100'>{item.JRFAsstProfTotal}</span>
-                    </div>
-                )
-            })
-        }
-    </div>
-  )
+    )
 }
 
 export default Table
